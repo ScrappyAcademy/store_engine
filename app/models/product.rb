8e0e9080a629d2601821dda2d4282fe835954f2e
Brian Kane <all.lucky.sevens@gmail.com>
2012-07-24 19:55:39 -0400

    [fixes #32807099] Add validations for product description

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,5 @@ class Product < ActiveRecord::Base
   attr_accessible :description, :price, :title
   validates :title, presence: true, length: {minimum: 3} , uniqueness: true 
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 0.0 }
+  validates :description, presence:true, length: {minimum: 1}
 end
