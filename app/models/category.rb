5b69938b49a928848a9812eada53fdfbdf76348a
Cory Cole <corypcole@gmail.com>
2012-07-31 20:48:17 -0400

    [finishes #32573607 #32886839] add name validation

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,5 @@ class Category < ActiveRecord::Base
   attr_accessible :name
   has_many :categorizations 
   has_many :products, through: :categorizations 
+  validates :name, presence: true, length: {minimum: 4, maximum: 20} , uniqueness: true 
 end
