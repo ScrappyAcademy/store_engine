3a550bc4b5b4c554d7b652faca6f1e76c9fbaeef
Cory Cole <corypcole@gmail.com>
2012-08-07 20:07:41 -0400

    [#32573751] Add category field

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ActiveRecord::Base
   attr_accessible :description, :price, :title, :retired
+  attr_accessible :category_list
   validates :title, presence: true, length: {minimum: 3} , uniqueness: true 
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 0.0 }
   validates :description, presence:true, length: {minimum: 1}
@@ -10,8 +11,27 @@ class Product < ActiveRecord::Base
 
   has_many :categories, through: :categorizations 
 
-  def category_list
+  def category_list 
     return self.categories.join(", ")
   end
+
+  def category_list=(categories_string)
+    categorizations.destroy_all
+    category_names = categories_string.split(",").collect{|s| s.strip.downcase}.uniq
+
+    category_names.each do |category_name|
+      category = Category.find_or_create_by_name(category_name)
+      categorizations = self.categorizations.new
+      categorizations.category_id = category.id
+    end 
+  end
+
+
+
+
+  
+
+
+
 
 end
