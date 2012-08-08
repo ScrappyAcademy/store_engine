67b908f2a2be538ce2b997288a059f7d72f4b7e3
Brian Kane <all.lucky.sevens@gmail.com>
2012-08-07 20:52:45 -0400

    [#32954109] Scaffold user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120731234737) do
+ActiveRecord::Schema.define(:version => 20120808004714) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
@@ -36,6 +36,15 @@ ActiveRecord::Schema.define(:version => 20120731234737) do
     t.datetime "created_at",                                :null => false
     t.datetime "updated_at",                                :null => false
     t.boolean  "retired"
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password"
+    t.string   "salt"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
 end
---
+++ spec/views/users/show.html.haml_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+describe "users/show" do
+  before(:each) do
+    @user = assign(:user, stub_model(User,
+      :name => "Name",
+      :email => "Email",
+      :password => "Password",
+      :salt => "Salt"
+    ))
+  end
+
+  it "renders attributes in <p>" do
+    render
+    # Run the generator again with the --webrat flag if you want to use webrat matchers
+    rendered.should match(/Name/)
+    rendered.should match(/Email/)
+    rendered.should match(/Password/)
+    rendered.should match(/Salt/)
+  end
+end
