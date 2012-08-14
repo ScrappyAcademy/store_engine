c4e91447d2d873e1abe3df946053be81059f2757
Aaron Kromer <aaron.kromer@gmail.com>
2012-08-14 19:20:02 -0400

    Update deprecated tags. Add test data.

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -24,7 +24,12 @@ describe UsersController do
   # User. As you add validations to User, be sure to
   # update the return value of this method accordingly.
   def valid_attributes
-    {}
+    {
+      name: "Example User",
+      email: "user@example.com",
+      password: "example",
+      salt: "encryption"
+    }
   end
 
   # This should return the minimal set of values that should be in the session
