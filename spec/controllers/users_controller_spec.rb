f0437441866a33516aed78b908d3fee310056d9a
Brian Kane <all.lucky.sevens@gmail.com>
2012-08-14 17:30:43 -0400

    [#32954109] Add user model test data

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -24,7 +24,7 @@ describe UsersController do
   # User. As you add validations to User, be sure to
   # update the return value of this method accordingly.
   def valid_attributes
-    {}
+    {name: "Sterling Archer", email: "dutchess@isis.net", password: "guest"}
   end
 
   # This should return the minimal set of values that should be in the session
