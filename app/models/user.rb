bc53463c12506877c263fa7b1b1a5b687387eb0e
Cory Cole <corypcole@gmail.com>
2012-08-13 22:46:26 -0400

    [#32954109] test presence of name, email, password,and salt.Test name length validation & email format validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :name, :password, :salt
+
+  validates :name, presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX}
+  validates :password, presence: true
+  validates :salt, presence: true 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,63 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  before do 
+  	@user = User.new(name: "Example User", email: "user@example.com", password: "example", salt: "encryption")
+	end
+  
+  subject {@user}
+
+  it { should respond_to(:name) }
+  it { should respond_to(:email) }
+ 
+
+  it { should be_valid }
+
+  describe "when name is not present" do 
+  	before { @user.name = " "}
+  	it { should_not be_valid}
+  end
+
+  describe "when name is too long" do
+    before { @user.name = "a" * 51}
+    it {should_not be_valid}
+  end 
+
+  describe "when email is not present" do
+    before { @user.email = " " }
+    it { should_not be_valid }
+  end
+
+  describe "when email format is invalid" do
+    it "should be invalid" do
+      addresses = %w[user@foo,com user_at_scrappy.org example.user@beast.foo@bar.com foo@bar+baz.com]
+      addresses.each do |invalid_address|
+        @user.email = invalid_address
+        @user.should_not be be_valid
+      end 
+    end 
+  end
+
+  describe "when email formait is valid" do
+    it "should be valid"  do 
+      addresses = %w[user@scrappy.COM a_US-er@f.b.org user.lst@bar.jp a+b@baz.cn]
+      addresses.each do |valid_address|
+        @user.email = valid_address
+        @user.should be_valid
+      end 
+    end
+  end
+
+  describe "when password is not present" do
+    before { @user.password = " " }
+    it { should_not be_valid}
+  end 
+
+   describe "when salt is not present" do
+    before { @user.salt = " " }
+    it { should_not be_valid}
+  end 
+
 end
+
