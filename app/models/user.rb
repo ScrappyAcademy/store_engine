719a1182f0fc59380233ea2b0e76e3b1d4a324e2
Brian Kane <all.lucky.sevens@gmail.com>
2012-08-14 17:30:04 -0400

    [#32954109] Replace password salt with has_secure_password on user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :name, :password, :salt
+  attr_accessible :email, :name, :password
+  has_secure_password
+  validates :email, presence: true
+  validates :name, presence: true
+  validates :password, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -41,10 +41,9 @@ ActiveRecord::Schema.define(:version => 20120808004714) do
   create_table "users", :force => true do |t|
     t.string   "name"
     t.string   "email"
-    t.string   "password"
-    t.string   "salt"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.string   "password_digest"
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
   end
 
 end
